2ea1a4d0b895a7d808d1e55f90a13fa1b37a86b8
Joel Wilsson <joel.wilsson@gmail.com>
2016-02-27 22:51:22 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,16 +3,19 @@
   :url "https://github.com/wjoel/reddit-spark"
   :license {:name "BSD 3-clause"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.3"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [yieldbot/flambo "0.7.1"]
                  [clj-kafka "0.3.3"]
-                 [org.apache.spark/spark-streaming_2.10 "1.5.2"]
-                 [org.apache.spark/spark-streaming-flume_2.10 "1.5.2"]
-                 [org.apache.spark/spark-streaming-kafka_2.10 "1.5.2"]
+                 [org.apache.spark/spark-streaming_2.11 "1.6.0"]
+                 [org.apache.spark/spark-streaming-flume_2.11 "1.6.0"]
+                 [org.apache.spark/spark-streaming-kafka_2.11 "1.6.0"]
                  [com.taoensso/carmine "2.12.0"]
                  [org.apache.lucene/lucene-core "5.3.1"]
                  [org.apache.lucene/lucene-analyzers-common "5.3.1"]]
   :main reddit-spark.core
-  :profiles {:provided {:dependencies [[org.apache.spark/spark-core_2.10 "1.5.2"]
+  :profiles {:provided {:dependencies [[org.apache.spark/spark-core_2.11 "1.6.0"]
                                        [org.apache.hadoop/hadoop-common "2.6.0"]]}
              :dev {:aot [reddit-spark.core]}
              :uberjar {:aot :all}})
